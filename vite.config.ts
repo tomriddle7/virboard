20461d79e882f064c011c38295133cb7200ba2ba
tomriddle7 <tomriddle7@naver.com>
2026-03-20 21:22:14 +0900

    Add alias&base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+const __dirname = path.resolve();
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      host: true,
+    },
+    build: {
+      sourcemap: false,
+    },
+    base: env.VITE_BASE_URL,
+  };
 })
