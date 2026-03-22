ef8e94954409d53b490fb1c8df6e45160d677d9e
tomriddle7 <tomriddle7@naver.com>
2026-03-22 14:17:51 +0900

    Mobile styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -56,43 +56,54 @@ function Home() {
   const closeSubmitModal = () => setSubmitOpen(false);
 
   return (
-    <main className="max-w-6xl mx-auto p-6 bg-gray-50 dark:bg-gray-950 min-h-screen">
-      <div className="flex justify-between items-center">
-        <button
-          className="px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md hover:bg-gray-50 dark:hover:bg-gray-800 transition invisible"
-        >제보하기</button>
-        <h1 className="text-center text-[#43c5f5] text-4xl font-bold mb-3">
-          버튜버 기념일 달력
-        </h1>
-        <button
-          className="px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md hover:bg-gray-50 dark:hover:bg-gray-800 transition"
-          onClick={() => {
-            setSubmitOpen(true);
-          }}>제보하기</button>
-      </div>
-      <div className="h-[800px] bg-white dark:bg-black p-6 rounded-2xl shadow-sm border border-gray-100 dark:border-gray-900">
-        <Calendar<VtuberEvent, object>
-          localizer={localizer}
-          events={events}
-          date={date}
-          onNavigate={(newDate) => setDate(newDate)}
-          onSelectEvent={(event) => setSelectedEvent(event)}
-          culture="ko"
-          views={["month"]}
-          popup={true}
-          components={{
-            toolbar: CustomToolbar,
-            event: CustomEvent,
-          }}
-          // 기본 인라인 배경색을 투명하게 만들어 Tailwind 클래스가 적용되게 해요
-          eventPropGetter={() => ({
-            style: { backgroundColor: "transparent" },
-          })}
-        />
-      </div>
-      {/* 4. 선택된 이벤트가 있을 때만 렌더링되는 모달 창 */}
-      {selectedEvent && <DetailPopup selectedEvent={selectedEvent} closeModal={closeDetailModal} />}
-      {submitOpen && <SubmitPopup closeModal={closeSubmitModal} />}
+    <main className="bg-gray-50 dark:bg-gray-950 min-h-screen">
+      <section className="max-w-6xl mx-auto pt-6 px-0 sm:px-6">
+        <div className="flex justify-between items-center">
+          <button
+            className="px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md hover:bg-gray-50 dark:hover:bg-gray-800 transition invisible"
+          >제보하기</button>
+          <h1 className="text-center text-[#43c5f5] text-4xl font-bold mb-3">
+            버튜버 기념일 달력
+          </h1>
+          <button
+            className="px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md hover:bg-gray-50 dark:hover:bg-gray-800 transition"
+            onClick={() => {
+              setSubmitOpen(true);
+            }}>제보하기</button>
+        </div>
+        <div className="h-[800px] bg-white dark:bg-black p-6 rounded-2xl shadow-sm border border-gray-100 dark:border-gray-900">
+          <Calendar<VtuberEvent, object>
+            localizer={localizer}
+            events={events}
+            date={date}
+            onNavigate={(newDate) => setDate(newDate)}
+            onSelectEvent={(event) => setSelectedEvent(event)}
+            culture="ko"
+            views={["month"]}
+            popup={true}
+            formats={{
+              dayHeaderFormat: (date) => {
+                return new Intl.DateTimeFormat('ko-KR', {
+                  month: 'long', 
+                  day: 'numeric',
+                  weekday: 'long'
+                }).format(date);
+              },
+            }}
+            components={{
+              toolbar: CustomToolbar,
+              event: CustomEvent,
+            }}
+            // 기본 인라인 배경색을 투명하게 만들어 Tailwind 클래스가 적용되게 해요
+            eventPropGetter={() => ({
+              style: { backgroundColor: "transparent" },
+            })}
+          />
+        </div>
+        {/* 4. 선택된 이벤트가 있을 때만 렌더링되는 모달 창 */}
+        {selectedEvent && <DetailPopup selectedEvent={selectedEvent} closeModal={closeDetailModal} />}
+        {submitOpen && <SubmitPopup closeModal={closeSubmitModal} />}
+      </section>
     </main>
   )
 }
